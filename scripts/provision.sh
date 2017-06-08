94d6e5265b5531bba5e440fa126cb8364957dd0e
Alan Monger <haveacigaro@gmail.com>
2017-06-08 21:32:29 +0100

    Install composer

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,6 +6,11 @@ sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum update -y
 sudo yum install vim nano nginx mariadb-server php71w-* --skip-broken -y
 
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/sbin --filename=composer
+php -r "unlink('composer-setup.php');"
+
 while read domain; do
     sudo sh /vagrant/scripts/add-domain.sh "${domain}"
 done </vagrant/domains.txt
