36960ec91144ba6f9a788eac686d0c338f3fc5d4
Alan Monger <haveacigaro@gmail.com>
2017-06-08 21:16:02 +0100

    Start mariadb server on start

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,10 +4,10 @@ sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 
 sudo yum update -y
-sudo yum install vim nano nginx php71w-* --skip-broken -y
+sudo yum install vim nano nginx mariadb-server php71w-* --skip-broken -y
 
 while read domain; do
-sudo sh /vagrant/scripts/add-domain.sh "${domain}"
+    sudo sh /vagrant/scripts/add-domain.sh "${domain}"
 done </vagrant/domains.txt
 
 sudo chmod -R 777 /var/log/php-fpm
@@ -16,5 +16,5 @@ sudo service php-fpm reload
 sudo chown nginx:nginx /run/*.sock
 sudo service php-fpm start
 sudo service nginx start
-
+sudo service mariadb start
 
