d35f25689d5fc8651bf96bb56aae216d93676a0a
Alan Monger <haveacigaro@gmail.com>
2017-06-08 23:10:50 +0100

    Add fortune

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,9 +2,10 @@
 
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
 
 sudo yum update -y
-sudo yum install vim nano nginx mariadb-server php71w-* --skip-broken -y
+sudo yum install fortune-mod cowsay vim nano ruby nodejs nginx mariadb-server php71w-* --skip-broken -y
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -22,4 +23,6 @@ sudo chown nginx:nginx /run/*.sock
 sudo service php-fpm start
 sudo service nginx start
 sudo service mariadb start
+
+fortune | cowsay -f stegosaurus
 
